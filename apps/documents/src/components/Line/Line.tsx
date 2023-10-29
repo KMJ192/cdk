5ff91ba0cfb8f6208113e7f2e36abe7863810ae9
MyeongJunKim <myeongjun222@naver.com>
2023-10-29 20:29:45 +0900

    feat(documents): 미사용 코드 제거

--- apps/documents/src/components/Line/Line.tsx
+++ apps/documents/src/components/Line/Line.tsx
@@ -1,15 +1,11 @@
 'use client';
 
-import useValueAppState from '@src/store/AppProvider/hooks/useValueAppState';
-
 import classNames from 'classnames/bind';
 import style from './style.module.scss';
 const cx = classNames.bind(style);
 
 function Line() {
-  const { theme } = useValueAppState();
-
-  return <div className={cx('line', theme)}></div>;
+  return <div className={cx('line')}></div>;
 }
 
 export default Line;
