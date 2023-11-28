45719eff140be012041330dec3dbdc7e3b533e1d
MyeongJunKim <myeongjun222@naver.com>
2023-11-28 16:53:06 +0900

    feat(documents): 준비 중 표시

--- apps/documents/src/app/modules/components/[slug]/page.tsx
+++ apps/documents/src/app/modules/components/[slug]/page.tsx
@@ -1,13 +1,16 @@
-import type { PAGE_MODULE_COMPONENTS } from '@src/utils/url';
+// import type { PAGE_MODULE_COMPONENTS } from '@src/utils/url';
 
-type Props = {
-  params: {
-    slug: PAGE_MODULE_COMPONENTS;
-  };
-};
+// type Props = {
+//   params: {
+//     slug: PAGE_MODULE_COMPONENTS;
+//   };
+// };
 
-function Page({ params }: Props) {
-  // return <div>{params.slug}</div>;
+// function Page({ params }: Props) {
+//   return <div>{params.slug}</div>;
+// }
+
+function Page() {
   return <div>준비중 입니다.</div>;
 }
 
--- apps/documents/src/app/modules/hooks/[slug]/page.tsx
+++ apps/documents/src/app/modules/hooks/[slug]/page.tsx
@@ -1,13 +1,16 @@
-import type { PAGE_HOOKS } from '@src/utils/url';
+// import type { PAGE_HOOKS } from '@src/utils/url';
 
-type Props = {
-  params: {
-    slug: PAGE_HOOKS;
-  };
-};
+// type Props = {
+//   params: {
+//     slug: PAGE_HOOKS;
+//   };
+// };
 
-function Page({ params }: Props) {
-  // return <div>{params.slug}</div>;
+// function Page({ params }: Props) {
+//   return <div>{params.slug}</div>;
+// }
+
+function Page() {
   return <div>준비중 입니다.</div>;
 }
 
