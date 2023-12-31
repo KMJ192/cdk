073231d458651a29d73aaab205cc6be569088387
MyeongJunKim <myeongjun222@naver.com>
2023-12-31 16:02:34 +0900

    feat(documents): Checkbox 문서 수정

--- apps/documents/src/appIsomorphic/ui/components/contents/checkbox.tsx
+++ apps/documents/src/appIsomorphic/ui/components/contents/checkbox.tsx
@@ -1,5 +1,4 @@
 import { Checkbox, Flex } from '@cdkit/react-ui';
-import { CSS_VAR_TYPE } from '@src/utils/utils';
 import type {
   CSS_VARIABLES,
   DATA_TYPE,
@@ -16,27 +15,39 @@ const document: Array<DOCUMENT> = [
     subtitle: 'Checkbox 예시입니다.',
     view: (
       <Flex className={style.checkbox}>
-        <Checkbox>unchecked</Checkbox>
-        <Checkbox checked>checked</Checkbox>
+        <Checkbox>
+          <Checkbox.MarkField>
+            <Checkbox.Mark></Checkbox.Mark>
+          </Checkbox.MarkField>
+        </Checkbox>
+        <Checkbox checked>
+          <Checkbox.MarkField>
+            <Checkbox.Mark></Checkbox.Mark>
+          </Checkbox.MarkField>
+        </Checkbox>
         <Checkbox checked multiple>
-          multiple
+          <Checkbox.MarkField>
+            <Checkbox.Mark></Checkbox.Mark>
+          </Checkbox.MarkField>
         </Checkbox>
-        <Checkbox disabled>disabled</Checkbox>
-        <Checkbox disabled checked>
-          disabled
+        <Checkbox checked disabled>
+          <Checkbox.MarkField>
+            <Checkbox.Mark></Checkbox.Mark>
+          </Checkbox.MarkField>
         </Checkbox>
-        <Checkbox disabled checked multiple>
-          disabled
+        <Checkbox checked multiple disabled>
+          <Checkbox.MarkField>
+            <Checkbox.Mark></Checkbox.Mark>
+          </Checkbox.MarkField>
         </Checkbox>
       </Flex>
     ),
     code: [
-      `<Checkbox>unchecked</Checkbox>`,
-      `<Checkbox checked>checked</Checkbox>`,
-      `<Checkbox checked multiple>multiple</Checkbox>`,
-      `<Checkbox disabled>disabled</Checkbox>`,
-      `<Checkbox disabled checked>disabled</Checkbox>`,
-      `<Checkbox disabled checked multiple>disabled</Checkbox>`,
+      `<Checkbox><Checkbox.MarkField><Checkbox.Mark></Checkbox.Mark></Checkbox.MarkField></Checkbox>`,
+      `<Checkbox checked><Checkbox.MarkField><Checkbox.Mark></Checkbox.Mark></Checkbox.MarkField></Checkbox>`,
+      `<Checkbox checked multiple><Checkbox.MarkField><Checkbox.Mark></Checkbox.Mark></Checkbox.MarkField></Checkbox>`,
+      `<Checkbox checked disabled><Checkbox.MarkField><Checkbox.Mark></Checkbox.Mark></Checkbox.MarkField></Checkbox>`,
+      `<Checkbox checked multiple disabled><Checkbox.MarkField><Checkbox.Mark></Checkbox.Mark></Checkbox.MarkField></Checkbox>`,
     ],
   },
 ];
@@ -45,7 +56,7 @@ const dataType: Array<DATA_TYPE> = [];
 
 const params: Array<PARAMS> = [
   {
-    title: 'Props',
+    title: 'Checkbox Props',
     defaultTag: 'div',
     element: [
       {
@@ -78,129 +89,35 @@ const params: Array<PARAMS> = [
       },
     ],
   },
-];
-
-const cssVar: Array<CSS_VARIABLES> = [
   {
-    title: 'CSS Variables',
+    title: 'MarkField Props',
+    defaultTag: 'div',
     element: [
       {
-        name: '--cdkit-color-checkbox-border',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['경계선 색상'],
-      },
-      {
-        name: '--cdkit-color-checkbox-border-hover',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['경계선 색상', '마우스 호버'],
-      },
-      {
-        name: '--cdkit-color-checkbox-border-disabled',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['경계선 색상', '비활성화'],
-      },
-      {
-        name: '--cdkit-color-checkbox-border-checked',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['경계선 색상', '선택됨'],
-      },
-      {
-        name: '--cdkit-color-checkbox-border-checked-hover',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['경계선 색상', '선택됨', '마우스 호버'],
-      },
-      {
-        name: '--cdkit-color-checkbox-border-checked-disabled',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['경계선 색상', '선택됨', '비활성화'],
-      },
-      {
-        name: '--cdkit-color-checkbox-bg',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['박스 배경 색상', '마킹된 경우만 적용'],
-      },
-      {
-        name: '--cdkit-color-checkbox-bg-hover',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['박스 배경 색상', '마우스 호버'],
-      },
-      {
-        name: '--cdkit-color-checkbox-bg-disabled',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['박스 배경 색상', '비활성화', '마킹되지 않더라도 적용'],
-      },
-      {
-        name: '--cdkit-color-checkbox-bg-checked-disabled',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['박스 배경 색상', '마킹', '비활성화'],
-      },
-      {
-        name: '--cdkit-color-checkbox-text',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['글자 색상'],
-      },
-      {
-        name: '--cdkit-color-checkbox-text-hover',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['글자 색상', '마우스 호버'],
-      },
-      {
-        name: '--cdkit-color-checkbox-text-disabled',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['글자 색상', '비활성화'],
-      },
-      {
-        name: '--cdkit-color-checkbox-mark',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['마킹 색상'],
-      },
-      {
-        name: '--cdkit-color-checkbox-mark-hover',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['마킹 색상', '마우스 호버'],
-      },
-      {
-        name: '--cdkit-color-checkbox-mark-disabled',
-        type: CSS_VAR_TYPE.COLOR,
-        description: ['마킹 색상', '비활성화'],
-      },
-      {
-        name: '--cdkit-size-checkbox',
-        type: CSS_VAR_TYPE.SIZE,
-        description: ['박스 크기'],
+        name: 'children',
+        type: 'React.ReactNode',
+        essential: false,
+        defaultValue: 'undefined',
+        description: ['Children 컴포넌트'],
       },
     ],
   },
 ];
 
+const cssVar: Array<CSS_VARIABLES> = [];
+
 const defaultCode: Array<string> = [
-  `<Checkbox checked={false} multiple={false} disabled={false}>Checkbox</Checkbox>`,
+  `<Checkbox>`,
+  `  <Checkbox.MarkField>`,
+  `    <Checkbox.Mark></Checkbox.Mark>`,
+  `  </Checkbox.MarkField>`,
+  `</Checkbox>`,
 ];
 
-const pgCode = `import { Checkbox, Spacing } from '@cdkit/react-ui';
+const pgCode = `import { Checkbox } from '@cdkit/react-ui';
 
 function App() {
-  return (
-    <>
-      <Checkbox>unchecked</Checkbox>
-      <Spacing spacing={8} />
-      <Checkbox checked>checked</Checkbox>
-      <Spacing spacing={8} />
-      <Checkbox checked multiple>
-        multiple
-      </Checkbox>
-      <Spacing spacing={8} />
-      <Checkbox disabled>disabled</Checkbox>
-      <Spacing spacing={8} />
-      <Checkbox disabled checked>
-        disabled
-      </Checkbox>
-      <Spacing spacing={8} />
-      <Checkbox disabled checked multiple>
-        disabled
-      </Checkbox>
-    </>
-  );
+  return <Checkbox></Checkbox>;
 }
 
 export default App;
