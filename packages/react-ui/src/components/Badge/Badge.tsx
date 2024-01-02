3f402e05cd4621a04549ec281542b74527a899cf
MyeongJunKim <myeongjun222@naver.com>
2024-01-02 23:23:18 +0900

    feat(react-ui): Badge 컴포넌트 수정

--- packages/react-ui/src/components/Badge/Badge.tsx
+++ packages/react-ui/src/components/Badge/Badge.tsx
@@ -12,12 +12,7 @@ const cx = classNames.bind(style);
 
 type BaseProps = {
   children?: React.ReactNode;
-  colorSchema?:
-    | 'primary'
-    | 'info'
-    | 'warning'
-    | 'danger'
-    | 'custom';
+  colorSchema?: 'primary' | 'success' | 'info' | 'warning' | 'danger';
   display?: CSS_DISPLAY;
   flexDirection?: CSS_DISPLAY_FLEX_DIRECTION;
   centerVertical?: boolean;
